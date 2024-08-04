14dfd14f7461ac8700b97854eb30bf21e507a22e
ChenKangLee <mark56333@gmail.com>
2024-08-04 16:08:19 -0700

    Guard image gallery behind auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 export default async function HomePage() {
@@ -8,13 +9,20 @@ export default async function HomePage() {
 
   return (
     <main className="">
-      <ul className="flex flex-wrap gap-2">
-        {images.map((image) => (
-          <ul key={image.id} className="w-48">
-            <img src={image.url} alt={image.name} />
-          </ul>
-        ))}
-      </ul>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          <h2>Please sign in to see the images related to you!</h2>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <ul className="flex flex-wrap gap-2">
+          {images.map((image) => (
+            <ul key={image.id} className="w-48">
+              <img src={image.url} alt={image.name} />
+            </ul>
+          ))}
+        </ul>
+      </SignedIn>
     </main>
   );
 }
